f087f305e4cca19ad6c606e2377fa2dfc60c9fdc
sweetkip <julilava10@gmail.com>
2026-05-04 17:12:26 -0300

    HOLA

--- Assets/Scripts/Car/HealthManager.cs
+++ Assets/Scripts/Car/HealthManager.cs
@@ -15,6 +15,11 @@ public class HealthManager : MonoBehaviour
     [Header("Death")]
     [SerializeField] private GameObject deathImage;
 
+    //Optimizacion
+    [Header("Heart")]
+    [SerializeField] private Transform heart;
+    [SerializeField] private Animator animator;
+
     private bool IsDead = false;
 
     private void Awake()
@@ -37,6 +42,7 @@ public class HealthManager : MonoBehaviour
         if (IsDead) return;
         health -= Mathf.Max(0f, damage);
         health = Mathf.Clamp(health, 0f, maxHealth);
+        animator.SetTrigger("Hit");
         UpdateUIInstant();
 
         if (health <= 0f && !IsDead)
@@ -48,6 +54,7 @@ public class HealthManager : MonoBehaviour
     private void UpdateUIInstant()
     {
         if (healthFillImage != null)
+            //Hola Lauti. El profe de optimización dice que con hacer el calculo de "health/maxHealth" está bien. No hace falta el Clamp porque lo hace solo el fillAmount.
             healthFillImage.fillAmount = Mathf.Clamp01(health / maxHealth);
     }
 
