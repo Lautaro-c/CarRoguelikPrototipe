53979bfe231208555892a57873afe1c041ec89f9
Lautaro-c <76079647+Lautaro-c@users.noreply.github.com>
2026-06-08 11:50:07 -0300

    Cambios previos a intentar implementar thetastar en enemycontroller

---
+++ Assets/Scripts/Enemies/Pathfinding/Node.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public List<Node> neightbourds;//<- Esto es lo unico que importa
+    //Si utilizan este codigo con los raycast en el start/update/realtime son un punto menos por raycast.
+    public bool hasTrap;
+    
+    public void SetTrap(bool hasTrap)
+    {
+        this.hasTrap = hasTrap;
+    }
+}
